665dfc6d3bd9071f650f172af87993f882ba6178
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-08-06 23:47:02 +0400

    structure

--- main.go
+++ main.go
@@ -8,6 +8,57 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	equasion, _ := reader.ReadString('\n')
-	fmt.Print(equasion)
+	input, error := reader.ReadString('\n')
+
+	if error != nil {
+		panic(error)
+	}
+
+	switch input[0] {
+	case 'I', 'V':
+		fmt.Println(toRoman(calc(parseRoman(input))))
+	}
+	panic(fmt.Errorf("Wrong input: %s", input))
+}
+
+func parse() (string, error) {
+	return "a", nil
+}
+
+func toRoman(givenResult int, err error) string {
+	if err != nil {
+		panic(err)
+	}
+	if givenResult <= 0 {
+		if givenResult == 0 {
+			panic("Result is zero")
+		} else {
+			panic("Result is negative")
+		}
+	} else {
+		return "V"
+	}
+}
+
+func parseRoman(string) (rune, int, int) {
+	return '+', 2, 2
+}
+
+func parseArabic(string) (rune, int, int) {
+	return '+', 2, 2
+}
+
+func calc(sign rune, first int, second int) (int, error) {
+	switch sign {
+	case '+':
+		return -1, nil
+	case '-':
+		return -1, nil
+	case '*':
+		return -1, nil
+	case '/':
+		return -1, nil
+	default:
+		return -1, fmt.Errorf("Unable to recognise sign %s", sign)
+	}
 }
