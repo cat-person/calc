a6fa616249f2295cb84e1f6eeb219f49ca64f079
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-08-07 01:16:03 +0400

    structure

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,11 +15,21 @@ func main() {
 		panic(error)
 	}
 
-	switch input[0] {
-	case 'I', 'V':
+	firstRune := input[0] // Should be sufficient
+
+	switch {
+	case firstRune == 'I', firstRune == 'V':
 		fmt.Println(toRoman(calc(parseRoman(input))))
+	case '0' <= firstRune && firstRune <= '9':
+		result, err := calc(parseArabic(input))
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println(result)
+		}
+	default:
+		panic(fmt.Errorf("Wrong input: %s", input))
 	}
-	panic(fmt.Errorf("Wrong input: %s", input))
 }
 
 func parse() (string, error) {
@@ -36,29 +47,74 @@ func toRoman(givenResult int, err error) string {
 			panic("Result is negative")
 		}
 	} else {
-		return "V"
+		vals := []int{100, 90, 50, 40, 10, 9, 5, 4, 1}
+		numerals := []string{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+		var result strings.Builder
+
+		for i := 0; i < len(vals); i++ {
+			for givenResult >= vals[i] {
+				givenResult -= vals[i]
+				result.WriteString(numerals[i])
+			}
+		}
+
+		return result.String()
 	}
 }
 
 func parseRoman(string) (rune, int, int) {
+
 	return '+', 2, 2
 }
 
-func parseArabic(string) (rune, int, int) {
-	return '+', 2, 2
+func parseArabic(givenEquasion string) (rune, int, int) {
+	first := 0
+	second := 0
+	sign := '@'
+
+	magicIdx := 0
+
+	for _, char := range givenEquasion {
+		switch {
+		case '0' <= char && char <= '9':
+			if magicIdx == 0 {
+				first = 10*first + int(char) - 48
+			} else if magicIdx == 2 {
+				second = 10*second + int(char) - 48
+			} else {
+				panic(fmt.Errorf("Unable to parse equasion %s", givenEquasion))
+			}
+		case char == '+' || char == '-' || char == '*' || char == '/':
+			if magicIdx == 1 {
+				sign = char
+			} else {
+				panic(fmt.Errorf("Unable to parse equasion %s", givenEquasion))
+			}
+		case char == ' ':
+			magicIdx++ // string shouldnt end here
+		}
+	}
+
+	if magicIdx != 2 {
+		panic(fmt.Errorf("Unable to parse equasion %s", givenEquasion))
+	}
+
+	return sign, first, second
 }
 
 func calc(sign rune, first int, second int) (int, error) {
+
 	switch sign {
 	case '+':
-		return -1, nil
+		return first + second, nil
 	case '-':
-		return -1, nil
+		return first - second, nil
 	case '*':
-		return -1, nil
+		return first * second, nil
 	case '/':
-		return -1, nil
+		return first / second, nil
 	default:
-		return -1, fmt.Errorf("Unable to recognise sign %s", sign)
+		return -1, fmt.Errorf(`Unable to recognise sign %c`, sign)
 	}
 }
